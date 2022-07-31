02c216ef1c10022c1b4e9b0f342df4a8f8a572cd
panda1986 <542638787@qq.com>
2022-07-31 17:39:36 +0800

    refine homepage icons

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -2,30 +2,33 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
 import Translate, {translate} from '@docusaurus/Translate';
+import easyToUse from '../../static/img/easy-to-use-xs.png';
+import foucsOn from '../../static/img/focus-on-xs.png';
+import highEfficiency from '../../static/img/high-efficiency-xs.png';
 
 const FeatureList = [
   {
     title: translate({id: 'homepage.easyToUseName'}),
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    Image: easyToUse,
     description: <Translate id='homepage.easyToUse' />,
   },
   {
     title: translate({id: 'homepage.focusOnName'}),
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    Image: foucsOn,
     description: <Translate id='homepage.focusOn' />,
   },
   {
     title: translate({id: 'homepage.highEfficiencyName'}),
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+    Image: highEfficiency,
     description: <Translate id='homepage.highEfficiency' />,
   },
 ];
 
-function Feature({ Svg, title, description }) {
+function Feature({ Image, title, description }) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
+        <img src={Image} className={styles.featureSvg} alt={title} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
