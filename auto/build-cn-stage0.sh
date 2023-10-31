c6186266d6b1854d088ca7519dced27b16779faa
winlin <winlinvip@gmail.com>
2023-10-31 17:41:01 +0800

    Update

--- auto/build-cn-stage0.sh
+++ auto/build-cn-stage0.sh
@@ -8,8 +8,9 @@ yarn cross-env REGION='zh-cn' BASE_URL='/lts/zh-cn/' \
   SEARCH_HTTPS_HOST=ossrs.net SEARCH_HTTPS_PORT=443 \
   SEARCH_HTTP_HOST=ossrs.net SEARCH_HTTP_PORT=80 \
   SEARCH_APIKEY=$SEARCH_APIKEY \
-  docusaurus build --locale zh-cn --out-dir build/zh-cn
+  docusaurus build --locale zh-cn --out-dir build/zh-cn &&
 
-(cd build && tar jcf zh-cn.tar.bz2 zh-cn)
+(cd build && tar cf zh-cn.tar zh-cn) &&
 du -sh build/*
 
+if [[ $? -ne 0 ]]; then echo "Build fail"; exit 1; fi
--- auto/build-en-stage1.sh
+++ auto/build-en-stage1.sh
@@ -8,8 +8,9 @@ yarn cross-env REGION='zh-cn' BASE_URL='/lts/en-us/' \
   SEARCH_HTTPS_HOST=ossrs.io SEARCH_HTTPS_PORT=443 \
   SEARCH_HTTP_HOST=ossrs.io SEARCH_HTTP_PORT=80 \
   SEARCH_APIKEY=$SEARCH_APIKEY \
-  docusaurus build --locale en-us --out-dir build/en-us
+  docusaurus build --locale en-us --out-dir build/en-us &&
 
-(cd build && tar jcf en-us.tar.bz2 en-us)
+(cd build && tar cf en-us.tar en-us) &&
 du -sh build/*
 
+if [[ $? -ne 0 ]]; then echo "Build fail"; exit 1; fi
