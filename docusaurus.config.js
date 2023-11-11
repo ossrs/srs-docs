2b599320398937c81463129776d9409de593a2d2
winlin <winlinvip@gmail.com>
2023-11-11 19:04:59 +0800

    Refine link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,6 +10,7 @@ const regionConfig = process.env.REGION === 'zh-cn' ? require('./config/zh-cn') 
 const url = process.env.URL || regionConfig.url;
 const baseUrl = process.env.BASE_URL || regionConfig.baseUrl;
 const defaultLocale = process.env.DEFAULT_LOCALE || regionConfig.defaultLocale;
+const sitePrefix = process.env.REGION === 'zh-cn' ? 'https://ossrs.net' : 'https://ossrs.io';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -171,7 +172,7 @@ const config = {
             position: 'right',
           },
           {
-            href: '../../gpt/g/g-FxEZ7G0XX-srs',
+            href: `${sitePrefix}/gpt/g/g-FxEZ7G0XX-srs`,
             label: 'AI Assistant',
             position: 'right',
           },
