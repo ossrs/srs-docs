450bfee69b33902e5823775b8b3c2f0cf02b104b
panda <542638787@qq.com>
2024-01-28 09:01:51 +0800

    srs-docs integrate with glime ai assistant (#64)
    * support glime assistant ai
    
    * add useIsBrowser check
    
    * refine code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useEffect} from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
@@ -41,6 +42,37 @@ function HomepageHeader() {
 }
 
 export default function Home() {
+  const isBrowser = useIsBrowser();
+
+  useEffect(() => {
+    const loadGlimeScript = () => {
+      const script = document.createElement('script');
+      script.src = 'https://cdn.glimelab.ai/widget/1.0.0/widget.js';
+
+      script.onload = () => {
+        // Call the window.glime.init function here
+        window.glime.init(`odGnkiPMumIMJp`, { styles: { baseFontSize: 16, theme: "dark" | "light" | "auto", hideCollapsedButton: false } });
+      };
+
+      document.head.appendChild(script);
+    };
+
+    if (isBrowser) {
+      loadGlimeScript();
+    }
+
+    return () => {
+      // Clean up any resources when the component unmounts
+      if (isBrowser) {
+        // Remove glime script if added to the DOM
+        const script = document.querySelector(`script[src="https://cdn.glimelab.ai/widget/1.0.0/widget.js"]`);
+        if (script) {
+          script.remove();
+        }
+      }
+    };
+  }, [isBrowser]);
+
   return (
     <Layout
       title={`SRS (Simple Realtime Server)`}
